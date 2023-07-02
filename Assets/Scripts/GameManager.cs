9646a8045b21c032885ee2081da7a30b3a08a87d
Amadeo Neto <45830880+AmadeoNeto@users.noreply.github.com>
2023-07-02 20:45:32 -0300

    Print metrics

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
   [SerializeField] QueensManipulator manipulator;
+
   [Header("Parameters")]
   [SerializeField] private int sizeOfPopulation = 100;
   [SerializeField] private int maxIterations = 10;
@@ -13,9 +16,17 @@ public class GameManager : MonoBehaviour
   [SerializeField] [Range(0,1)]  private float crossoverProb = 0.9f;
   [SerializeField] int splitsNumber = 1; 
   [SerializeField] private int[] geneSlices = { 2, 4, 6 };
+  [SerializeField] private int retries = 4;
 
   public BoardSetting[] boardSettings;
   private BoardSetting bestSetting;
+
+    //Metrics
+    private int[] nIterations;
+    private bool[] iterConverged;
+    private int[] nConvergedPops;
+    private double[] avgFitness;
+    private BoardSetting[] bestPop;
 
   //Return the best setting among the various boardSettings
   BoardSetting GetBestSetting(BoardSetting[] boardSettings)
@@ -43,45 +54,69 @@ public class GameManager : MonoBehaviour
 
   public void OnRun()
   {
-    int iter = 0;
-    bestSetting = null;
-    int bestFitness = int.MaxValue;
-    boardSettings = new BoardSetting[sizeOfPopulation];
+    //Reset Metrics
+    nIterations = new int[retries];
+    iterConverged = new bool[retries];
+    nConvergedPops = new int[retries];
+    avgFitness = new double[retries];
+    bestPop = new BoardSetting[retries];
 
-    //Initialize the population with random individuals
-    for (int i = 0; i < sizeOfPopulation; i++)
+    for (int i=0; i<retries; i++)
     {
-      boardSettings[i] = new BoardSetting();
+      Epoch(i);
     }
 
-    while (bestFitness != 0 && iter < maxIterations)
-    {
-      var parents = GeneticManager.ChooseParents(boardSettings);
-      Debug.Log("p1: " + parents.parent1.ToString());
-      Debug.Log("p2: " + parents.parent2.ToString());
-
-      var offspring = GeneticManager.GenerateOffspring(parents.parent1, parents.parent2, offspringSize,
-                                                        splitsNumber,geneSlices,mutationProb,crossoverProb);
-
-      Debug.Log("Offspring: " + offspring[0] + " \n " + offspring[1]);
-
-      boardSettings = GeneticManager.SurvivorSelection(boardSettings, offspring);
-
-      //Update best setting
-      bestSetting = GetBestSetting(boardSettings);
-      bestFitness = bestSetting.Fitness;
-
-      //Update board renderer
-      manipulator.OnUpdateQueens(bestSetting);
-
-      PrintPopulationFitness();
-      iter++;
-    }
-
-    Debug.Log("Acabou o loop!" + "\nCollisions: " + bestSetting.Fitness.ToString());
-    Debug.Log("(" + iter + ") best: " + bestSetting.Fitness);
-    Debug.Log("Iterations: " + iter.ToString());
+     PrintMetrics();
   }
+
+    private void Epoch(int epochNum)
+    {
+        int iter = 0;
+        bestSetting = null;
+        int bestFitness = int.MaxValue;
+        boardSettings = new BoardSetting[sizeOfPopulation];
+
+        //Initialize the population with random individuals
+        for (int i = 0; i < sizeOfPopulation; i++)
+        {
+            boardSettings[i] = new BoardSetting();
+        }
+
+        while (bestFitness != 0 && iter < maxIterations)
+        {
+            var parents = GeneticManager.ChooseParents(boardSettings);
+            Debug.Log("p1: " + parents.parent1.ToString());
+            Debug.Log("p2: " + parents.parent2.ToString());
+
+            var offspring = GeneticManager.GenerateOffspring(parents.parent1, parents.parent2, offspringSize,
+                                                              splitsNumber, geneSlices, mutationProb, crossoverProb);
+
+            Debug.Log("Offspring: " + offspring[0] + " \n " + offspring[1]);
+
+            boardSettings = GeneticManager.SurvivorSelection(boardSettings, offspring);
+
+            //Update best setting
+            bestSetting = GetBestSetting(boardSettings);
+            bestFitness = bestSetting.Fitness;
+
+            //Update board renderer
+            manipulator.OnUpdateQueens(bestSetting);
+
+            PrintPopulationFitness();
+            iter++;
+        }
+
+        Debug.Log("Acabou o loop!" + "\nCollisions: " + bestSetting.Fitness.ToString());
+        Debug.Log("(" + iter + ") best: " + bestSetting.Fitness);
+        Debug.Log("Iterations: " + iter.ToString());
+
+        //Setting metric fields
+        nIterations[epochNum] = iter;
+        iterConverged[epochNum] = boardSettings.Any(board => board.Fitness == 0);
+        nConvergedPops[epochNum] = boardSettings.Count(board => board.Fitness == 0);
+        avgFitness[epochNum] = boardSettings.Average(board => board.Fitness);
+        bestPop[epochNum] = bestSetting;
+    }
 
   void PrintPopulationFitness(){
       //Debug method used to print the fitness of each individual from the population 
@@ -101,4 +136,46 @@ public class GameManager : MonoBehaviour
 
       Debug.Log(tmpTxt);
   }
+
+  void PrintMetrics()
+    {
+        double convergenceCount = iterConverged.Count(conv => conv==true);
+
+        double avgIterNum = nIterations.Average();
+        double stdIterNum = Std(nIterations);
+
+        double avgFit = avgFitness.Average();
+        double stdFit = Std(avgFitness);
+
+
+        string printTxt = $"Convergence Rate: {convergenceCount/retries}\n" +
+            $"Nº Iterations: avg={avgIterNum} | std={stdIterNum}\n" +
+            $"Fitness: avg={avgFit} | std={stdFit}"; 
+
+        Debug.Log(printTxt);
+    }
+
+    double Std(double[] array)
+    {
+        double avg = array.Average();
+        int count = array.Length;
+        double sqrDiff = 0;
+
+        foreach(int x in array)
+        {
+            sqrDiff += Math.Pow((x - avg),2);
+        }
+
+        return Math.Sqrt(sqrDiff / (count - 1));
+    }
+
+    double Std(int[] array)
+    {
+        double[] convertedArray = new double[array.Length];
+        for(int i=0; i < array.Length; i++)
+        {
+            convertedArray[i] = (double)array[i];
+        }
+        return Std(convertedArray);
+    }
 }
